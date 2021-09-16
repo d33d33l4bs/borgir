3dbc098b1b5f300861d3f23ccf249251827b11f6
Faust <>
2021-09-16 18:17:05 +0200

    Remove hardcoded guild

--- src/borgir/__main__.py
+++ src/borgir/__main__.py
@@ -11,12 +11,13 @@ DEFAULT_EXTENSIONS = [
 def main():
     parser = argparse.ArgumentParser(description="Borgir!")
     parser.add_argument("--discord-token", required=True)
+    parser.add_argument("--guild", required=True)
     parser.add_argument("--command-channel", required=True)
     parser.add_argument("--command-prefix", default="!")
     parser.add_argument("--extensions", default=DEFAULT_EXTENSIONS)
     args = parser.parse_args()
 
-    bot = Borgir(args.command_channel, args.command_prefix)
+    bot = Borgir(args.guild, args.command_channel, args.command_prefix)
     for extension in args.extensions:
         bot.load_extension(extension)
     bot.run(args.discord_token)
--- src/borgir/bot.py
+++ src/borgir/bot.py
@@ -3,16 +3,16 @@ from discord.utils import get
 
 
 class Borgir(commands.Bot):
-    def __init__(self, command_channel_name, *args, **kwargs):
+    def __init__(self, guild_name, command_channel_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.command_channel = None
+        self._guild_name = guild_name
         self._command_channel_name = command_channel_name
 
     async def on_ready(self):
-        # TODO: handle multi guilds?
-        guild = self.guilds[0]
+        guild = get(self.guilds, name=self._guild_name)
         self.command_channel = get(guild.text_channels, name=self._command_channel_name)
-        print(f"{self.user} is now connected.")
+        print(f"{self.user} is now connected to the guild {self._guild_name}.")
 
     async def on_error(self, event, *args, **kwargs):
         pass
